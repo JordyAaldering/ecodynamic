4f449060344f0c119b6a961bc38dd0192da62086
Jordy Aaldering <aalderingjordy@gmail.com>
2024-08-15 11:59:54 +0200

    seems to work pretty decently... :)

--- src/controller.rs
+++ src/controller.rs
@@ -1,13 +1,16 @@
 mod direction;
 mod selection;
 
+use std::u64;
+
 use crate::letterbox::Sample;
 use direction::Direction;
 use selection::*;
 
 pub struct Controller {
     n: i32,
-    t1: u64,
+    t_best: u64,
+    t_best_thread_count: u64,
     t_last: u64,
     step_size: i32,
     step_direction: Direction,
@@ -22,7 +25,8 @@ impl Controller {
     pub fn new(max_threads: i32) -> Controller {
         Controller {
             n: max_threads,
-            t1: u64::MAX,
+            t_best: u64::MAX,
+            t_best_thread_count: u64::MAX,
             t_last: u64::MAX,
             step_size: max_threads,
             step_direction: Direction::Down,
@@ -38,17 +42,26 @@ impl Controller {
         let samples = samples.into_iter().map(self.sample_value_selector).collect();
         let tn = self.selection_algorithm.find_best(samples);
 
-        let speedup = self.t1 as f64 / tn as f64;
+        let speedup = self.t_best as f64 / tn as f64;
         if speedup < 1.0 - self.corridor_width {
-            // We have fallen outside the corridor
-            self.step_direction = Direction::Down;
+            println!("Fallen outside the corridor (speedup = {})", speedup);
+            //self.step_direction = Direction::Down;
+            // Move up or down depending on where the best thread count was
+            self.step_direction = if self.n as u64 >= self.t_best_thread_count
+                { Direction::Down } else { Direction::Up };
             self.step_size = i32::max(1, self.n / 2);
         } else {
-            if tn < self.t1 {
+            if speedup > 1.0 / (1.0 - self.corridor_width) {
+                println!("Went above the corridor (speedup = {})", speedup);
+                self.step_size = i32::max(1, self.n / 2);
+            }
+
+            if tn < self.t_best {
                 // In the initial iteration t1 and t_last as u64::MAX so we
                 // reach this condition, an initialize t1 with a real value
-                println!("Approximation of t1 updated to {}", tn);
-                self.t1 = tn;
+                println!("T_best updated to {} at {} threads", tn, self.n);
+                self.t_best = tn;
+                self.t_best_thread_count = self.n as u64;
             }
 
             if tn > self.t_last {
@@ -69,7 +82,7 @@ impl Controller {
         let samples = samples.into_iter().map(|sample| sample.realtime_ns).collect();
         let tn = self.selection_algorithm.find_best(samples);
 
-        let speedup = self.t1 as f64 / tn as f64;
+        let speedup = self.t_best as f64 / tn as f64;
         if speedup < (1.0 - self.corridor_width) * self.n as f64 {
             // We have fallen outside the corridor
             self.step_direction = Direction::Down;
@@ -79,7 +92,7 @@ impl Controller {
                 // In the initial iteration t1 and t_last as u64::MAX so we
                 // reach this condition, an initialize t1 with a real value
                 println!("Approximation of t1 updated to {}", tn * self.n as u64);
-                self.t1 = tn * self.n as u64;
+                self.t_best = tn * self.n as u64;
             }
 
             if tn > self.t_last {
@@ -97,5 +110,11 @@ impl Controller {
     fn next_n(&self) -> i32 {
         let n = self.n + self.step_direction * self.step_size;
         i32::max(1, i32::min(self.max_threads, n))
+    }
+}
+
+impl std::fmt::Debug for Controller {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("T_best: {}, {} threads", self.t_best, self.t_best_thread_count))
     }
 }
