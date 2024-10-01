a7466eb35dc9ffe5968b639bcba0c0e02530bdfa
Jordy Aaldering <aalderingjordy@gmail.com>
2024-10-01 09:51:46 +0200

    adaptation quality test

---
+++ examples/matmul_resize.rs
@@ -0,0 +1,135 @@
+use std::{hint::black_box, time::Instant};
+
+use cpu_time::ProcessTime;
+use mtdynamic::MTDynamic;
+use rand::Rng;
+use rapl_energy::Rapl;
+use rayon::prelude::*;
+
+struct Matrix {
+    rows: usize,
+    cols: usize,
+    data: Vec<Vec<f64>>,
+}
+
+impl Matrix {
+    fn new(data: Vec<Vec<f64>>) -> Self {
+        Matrix {
+            rows: data.len(),
+            cols: data[0].len(),
+            data,
+        }
+    }
+
+    fn random(x: usize, y: usize) -> Self {
+        let mut rng = rand::thread_rng();
+        let data = (0..y).map(|_| {
+            let mut row = vec![0.0; x];
+            rng.fill(row.as_mut_slice());
+            row
+        }).collect();
+        Self::new(data)
+    }
+
+    fn mul(&self, other: &Matrix) -> Matrix {
+        let mut res = vec![vec![0.0; other.cols]; self.rows];
+
+        res.par_iter_mut().enumerate().for_each(|(row_a, data)| {
+            for col_b in 0..other.cols {
+                for i in 0..self.cols {
+                    data[col_b] += self.data[row_a][i] * other.data[i][col_b];
+                }
+            }
+        });
+
+        Matrix::new(res)
+    }
+}
+
+fn create_pool(num_threads: usize) -> rayon::ThreadPool {
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(num_threads)
+        .build()
+        .unwrap()
+}
+
+fn create_pool_pinned(num_threads: usize) -> rayon::ThreadPool {
+    let cores = core_affinity::get_core_ids().unwrap();
+    let max_threads = cores.len();
+    assert!(num_threads <= max_threads);
+    let thread_indices: Vec<usize> = (0..max_threads).step_by(2)
+        .chain((1..max_threads).step_by(2)).collect();
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(num_threads)
+        .start_handler(move |idx| {
+            let thread_idx = thread_indices[idx];
+            let core_id = cores[thread_idx];
+            assert!(core_affinity::set_for_current(core_id));
+        })
+        .build()
+        .unwrap()
+}
+
+fn main() {
+    let mut mtd = MTDynamic::new(16, 20);
+    let mut rapl = Rapl::now().unwrap();
+
+    const CYCLES: [(usize, bool); 20] = [
+        (500, false),
+        (750, false),
+        (1000, false),
+        (1250, false),
+        (1500, false),
+        (500, true),
+        (750, true),
+        (1000, true),
+        (1250, true),
+        (1500, true),
+        (500, false),
+        (750, false),
+        (1000, false),
+        (1250, false),
+        (1500, false),
+        (500, true),
+        (750, true),
+        (1000, true),
+        (1250, true),
+        (1500, true),
+    ];
+
+    println!("size,pin,runtime,usertime,energy");
+
+    let mut num_threads = 16;
+    for (size, pin) in CYCLES {
+        let mut pool = if pin { create_pool_pinned(num_threads) } else { create_pool(num_threads) };
+
+        let x = black_box(Matrix::random(size, size));
+        let y = black_box(Matrix::random(size, size));
+
+        for _ in 0..100 {
+            let _ = rapl.elapsed_mut();
+            let user = ProcessTime::now();
+            let real = Instant::now();
+
+            pool.install(|| {
+                let _ = black_box(x.mul(&y));
+            });
+
+            let real = real.elapsed();
+            let user = user.elapsed();
+            let rapl = rapl.elapsed_mut();
+
+            let real = real.as_secs_f64();
+            let user = user.as_secs_f64();
+            let rapl = rapl.values().sum();
+
+            println!("{},{},{:.8},{:.8},{:.8}", size, pin, real, user, rapl);
+
+            mtd.update("parallel", real, user, rapl);
+            num_threads = mtd.num_threads("parallel") as usize;
+            if pool.current_num_threads() != num_threads {
+                pool = if pin { create_pool_pinned(num_threads) } else { create_pool(num_threads) };
+            }
+        }
+    }
+}
---
+++ matmul_resize.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#SBATCH --account=csmpi
+#SBATCH --partition=csmpi_long
+#SBATCH --nodelist cn128
+#SBATCH --mem=0
+#SBATCH --cpus-per-task=16
+#SBATCH --time=10:00:00
+#SBATCH --output=cn128_matmul_resize.out
+
+cargo run --release --example matmul_resize
