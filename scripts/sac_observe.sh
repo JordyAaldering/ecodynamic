89206b7f361b41ccecb1b4b42a04814eafb70e1c
Jordy Aaldering <aalderingjordy@gmail.com>
2024-10-08 11:45:00 +0200

    sac matmul

--- scripts/sac_observe.sh
+++ scripts/sac_observe.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=0
 #SBATCH --cpus-per-task=16
 #SBATCH --time=10:00:00
-#SBATCH --output=sac_find_best.out
+#SBATCH --output=sac_observe.out
 
 ITER=20
 
@@ -15,31 +15,30 @@ printf "pin,size,threads,,energy,runtime,usertime\n"
 # With pinning
 for size in `seq 500 50 1250`; do
     ../sac2c/build_r/sac2c_p -noprelude -t mt_pth -mt_bind simple matmul.sac -o matmul -DP=$size -DITER=$ITER
-    for threads in `seq 4 16`; do
+    for threads in `seq 1 16`; do
         printf "true,$size,$threads,"
         ./matmul -mt $threads
     done
 done
 for size in `seq 1500 250 2500`; do
     ../sac2c/build_r/sac2c_p -noprelude -t mt_pth -mt_bind simple matmul.sac -o matmul -DP=$size -DITER=$ITER
-    for threads in `seq 4 16`; do
+    for threads in `seq 1 16`; do
         printf "true,$size,$threads,"
         ./matmul -mt $threads
     done
 done
 
 # Without pinning
-
 for size in `seq 500 50 1250`; do
     ../sac2c/build_r/sac2c_p -noprelude -t mt_pth matmul.sac -o matmul -DP=$size -DITER=$ITER
-    for threads in `seq 4 16`; do
+    for threads in `seq 1 16`; do
         printf "false,$size,$threads,"
         ./matmul -mt $threads
     done
 done
 for size in `seq 1500 250 2500`; do
     ../sac2c/build_r/sac2c_p -noprelude -t mt_pth matmul.sac -o matmul -DP=$size -DITER=$ITER
-    for threads in `seq 4 16`; do
+    for threads in `seq 1 16`; do
         printf "false,$size,$threads,"
         ./matmul -mt $threads
     done
