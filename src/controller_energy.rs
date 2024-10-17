f1a1e3a9a7bf4364e2cb2b98481e844dad8b74fe
Jordy Aaldering <aalderingjordy@gmail.com>
2024-10-17 13:44:56 +0200

    undo

--- src/controller_energy.rs
+++ src/controller_energy.rs
@@ -33,7 +33,7 @@ impl Controller for ControllerEnergy {
             self.step_direction = towards_farthest_edge(*self.n, self.max_threads);
             self.step_size = self.max_threads * 0.5;
         } else {
-            if tn > self.t_last * 1.01 {
+            if tn > self.t_last * 1.02 {
                 // Previous iteration performed (a bit) better
                 self.step_direction = -self.step_direction;
             }
@@ -46,7 +46,7 @@ impl Controller for ControllerEnergy {
             }
         }
 
-        if tn < self.t_last || tn > self.t_last * 1.01 {
+        if tn < self.t_last || tn > self.t_last * 1.02 {
             // Only update after a significant change
             self.t_last = tn;
         }
