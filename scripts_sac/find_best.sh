34798de5fbab1b4c1680ea5b1f325b95ca9864f9
Jordy Aaldering <aalderingjordy@gmail.com>
2024-10-23 17:19:08 +0200

    move scripts

--- scripts_sac/find_best.sh
+++ scripts_sac/find_best.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --account=csmpi
 #SBATCH --partition=csmpi_long
-#SBATCH --nodelist=cn128
+#SBATCH --nodelist=cn127
 #SBATCH --mem=0
 #SBATCH --cpus-per-task=16
 #SBATCH --time=10:00:00
@@ -15,9 +15,9 @@ printf "pin,size,threads,runtime,runtimesd,energy,energysd,name\n"
 for pin in true false; do
     for size in `seq 500 100 1500`; do
         if [ $pin ]; then
-            ../sac2c/build_r/sac2c_p -noprelude -specmode akd -sigspec akd -t mt_pth -mt_bind simple scripts/matmul.sac -o matmul -DP=$size -DITER=$ITER
+            ../sac2c/build_r/sac2c_p -noprelude -specmode akd -sigspec akd -t mt_pth -mt_bind simple scripts_sac/matmul.sac -o matmul -DP=$size -DITER=$ITER
         else
-            ../sac2c/build_r/sac2c_p -noprelude -specmode akd -sigspec akd -t mt_pth scripts/matmul.sac -o matmul -DP=$size -DITER=$ITER
+            ../sac2c/build_r/sac2c_p -noprelude -specmode akd -sigspec akd -t mt_pth scripts_sac/matmul.sac -o matmul -DP=$size -DITER=$ITER
         fi
 
         for threads in `seq 1 16`; do
