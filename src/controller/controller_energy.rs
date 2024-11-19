1bbcc69bdd591af2f446407ccddd2f624de1dc2b
Jordy Aaldering <aalderingjordy@gmail.com>
2024-11-19 14:02:41 +0100

    undo round

--- src/controller/controller_energy.rs
+++ src/controller/controller_energy.rs
@@ -1,34 +1,33 @@
 use super::{direction::Direction, thread_count::ThreadCount, Controller};
 
 pub struct EnergyController {
-    num_threads: ThreadCount,
+    n: ThreadCount,
     max_threads: f32,
     step_size: f32,
     step_direction: Direction,
-    e_prev: f32,
+    t_last: f32,
 }
 
 impl EnergyController {
     pub fn new(max_threads: usize) -> Self {
         Self {
-            num_threads: ThreadCount::new(max_threads),
+            n: ThreadCount::new(max_threads),
             max_threads: max_threads as f32,
             step_size: max_threads as f32,
             step_direction: Direction::Down,
-            e_prev: 0.0,
+            t_last: 0.0,
         }
     }
 }
 
 impl Controller for EnergyController {
-    fn adjust_threads(&mut self, e_avg: f32) -> f32 {
-        let e_avg = round_digits(e_avg, 2);
-        if e_avg > self.e_prev * 1.50 {
+    fn adjust_threads(&mut self, tn: f32) -> f32 {
+        if tn > self.t_last * 1.50 {
             // Previous iteration performed a lot better
-            self.step_direction = towards_farthest_edge(*self.num_threads, self.max_threads);
+            self.step_direction = towards_farthest_edge(*self.n, self.max_threads);
             self.step_size = self.max_threads * 0.5;
         } else {
-            if e_avg > self.e_prev {
+            if tn > self.t_last * 1.02 {
                 // Previous iteration performed (a bit) better
                 self.step_direction = -self.step_direction;
             }
@@ -36,14 +35,18 @@ impl Controller for EnergyController {
             if self.step_size > 0.16 {
                 self.step_size = f32::max(self.step_size * 0.6, self.step_size / (0.85 + self.step_size));
             } else {
-                self.step_direction = towards_farthest_edge(*self.num_threads, self.max_threads);
+                self.step_direction = towards_farthest_edge(*self.n, self.max_threads);
                 self.step_size = self.max_threads * 0.5;
             }
         }
 
-        self.e_prev = e_avg;
-        self.num_threads += self.step_direction * self.step_size;
-        *self.num_threads as f32
+        if tn < self.t_last || tn > self.t_last * 1.02 {
+            // Only update after a significant change
+            self.t_last = tn;
+        }
+
+        self.n += self.step_direction * self.step_size;
+        *self.n as f32
     }
 }
 
@@ -56,10 +59,4 @@ fn towards_farthest_edge(n: f32, max_threads: f32) -> Direction {
     } else {
         Direction::Down
     }
-}
-
-#[inline]
-fn round_digits(x: f32, decimals: u32) -> f32 {
-    let y = 10u32.pow(decimals) as f32;
-    (x * y).round() / y
 }
