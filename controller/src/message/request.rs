274fc932a29974a3e101b48f8c5eb7fcfc053b14
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-04-16 16:28:09 +0200

    make fields optional

--- controller/src/message/request.rs
+++ controller/src/message/request.rs
@@ -1,5 +1,3 @@
-use std::mem;
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -7,5 +5,6 @@ pub struct Request {
     /// A unique identifier of the parallel region we are controlling.
     pub region_uid: i32,
     /// The same region might have a varying input size.
-    pub problem_size: i32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub problem_size: Option<i32>,
 }
