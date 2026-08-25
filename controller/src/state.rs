c823886b8da301416bb08488c874fc235e9f69f7
Jordy Aaldering <33897257+JordyAaldering@users.noreply.github.com>
2026-08-25 15:17:03 +0200

    initial idea for score nudging

--- controller/src/state.rs
+++ controller/src/state.rs
@@ -7,4 +7,4 @@ use std::sync::atomic::AtomicU16;
 pub static GLOBAL_THREAD_COUNT: AtomicU16 = AtomicU16::new(0);
 
 /// Temporary value, until I convert this to a proper configuration parameter.
-pub(crate) const AVAILABLE_CORES: u16 = 16;
+pub(crate) const AVAILABLE_CORES: u16 = 8;
